e0740c7e9b2dea91e1ae01e5f1ad01ca2833fe43
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-01-30 12:23:39 +0300

    clients form call-back

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
 
 	$('#project-cards-list').mixItUp()
 
+	$('.form-callback__input, .form-callback__textarea').focus(function (e) {
+		e.preventDefault()
+
+		$(this).parent().children(".fake-placeholder").addClass('active')
+	})
+
+	$('.form-callback__input, .form-callback__textarea').blur(function (e) {
+		e.preventDefault()
+
+		$(this).parent().children(".fake-placeholder").removeClass('active')
+	})
 })
